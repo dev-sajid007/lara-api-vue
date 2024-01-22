28b4262c53abbba806ddbf8bf18e1317804f2b7d
dev-sajid007 <dev.sajid007@gmsil.com>
2024-01-22 15:28:11 +0600

    Getting started with Laravel 10 (Build REST API)

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TaskStoreRequest;
+use App\Http\Requests\TaskUpdateRequest;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return TaskResource::collection(Task::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(TaskStoreRequest $request)
+    {
+        $task = Task::create($request->validated());
+        
+        return TaskResource::make($task);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return TaskResource::make($task);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(TaskUpdateRequest $request, Task $task)
+    {
+        $task->update($request->validated());
+
+        return TaskResource::make($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return response()->noContent();
+    }
+}
